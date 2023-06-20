e0076396375d4af07973861199dea92733a7839d
WilmerHTC <wilmerchavavo@gmail.com>
2023-06-20 15:22:04 -0500

    Mas cambios

--- app/app.js
+++ app/app.js
@@ -60,7 +60,7 @@ app.use(
 app.use(express.static("guardar"));
 
 app.get("/", (req, res) => {
-  res.json("probando la ruta de inicio de mi app.");
+  res.json("probando la ruta de inicio de mi aplicacioó.");
 });
 
 // dbconnection.connect((error) => {
