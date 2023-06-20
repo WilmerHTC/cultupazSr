2a200d4e1277fc0ffd5d1bf1aabc7828587ebfa5
WilmerHTC <wilmerchavavo@gmail.com>
2023-06-20 11:02:24 -0500

    Cambios en index

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import { PORT } from "./app/config.js";
 import app from "./app/app.js";
 
-const port = process.env.PORT || PORT;
-app.listen(port, () => {
-  console.log(`http://localhost:${port}`);
+// const port = process.env.PORT || 7000;
+app.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}`);
 });
